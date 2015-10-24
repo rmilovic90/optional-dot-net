7d9377f182a2af5efca2e6aa98bcbbb350074b0f
r.milovic <r.milovic90@gmail.com>
2015-10-24 22:18:26 +0200

    Initial commit.

---
+++ src/Optional.NET.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Optional.NET Tests")]
+[assembly: AssemblyDescription("Tests for Optional.NET library.")]
+[assembly: AssemblyVersion("0.0.1")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ef5bc98f-3a63-45ac-9e7e-50318b21340f")]
---
+++ src/Optional.NET/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Optional.NET")]
+[assembly: AssemblyDescription(".NET (C#) implementation of Java 8 Optional type.")]
+[assembly: AssemblyVersion("0.0.1")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("aafab5a0-a085-44db-90b1-fe3ef1c4b44d")]
